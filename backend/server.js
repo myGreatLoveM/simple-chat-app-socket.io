86856f593dc7372af9002ddae8818a2df450f199
prem <praulayar994@gmail.com>
2024-03-19 20:56:47 +0530

    deployment

--- backend/server.js
+++ backend/server.js
@@ -6,9 +6,11 @@ import connectDB from './db/connectDb.js'
 import authRoutes from './routes/auth.routes.js'
 import messageRoutes from './routes/message.route.js'
 import userRoutes from './routes/user.route.js'
+import path from 'path'
 
-dotenv.config()
 const PORT = process.env.PORT || 3000
+const __dirname = path.resolve()
+dotenv.config()
 
 app.use(express.json())
 app.use(cookieParser())
@@ -17,11 +19,14 @@ app.use('/api/auth', authRoutes)
 app.use('/api/messages', messageRoutes)
 app.use('/api/users', userRoutes)
 
+app.use(express.static(path.join(__dirname, '/frontend/dist')))
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'frontend', 'dist', 'index.html'))
+})
+
 server.listen(PORT, () => {
   connectDB()
   console.log(`Server listening on port ${PORT}`)
 })
 
-app.get('/', (req, res) => {
-  res.send('Welcome!')
-})
